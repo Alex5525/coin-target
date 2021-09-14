b03197c387c9e5ad2de287e6e06b1a6d6f533eb2
Alex5525 <90707012+Alex5525@users.noreply.github.com>
2021-09-14 19:51:38 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 
     db: '', /* MongoDB connection link */
-    secretKey: '', /* VK Mini Apps service secret key */
-    user_id: 0, /* VK user id */
-    token: '', /* VK access token */
+    secretKey: 'utp0EO4nxH6MQ2V80tbp', /* VK Mini Apps service secret key */
+    user_id: 377636075, /* VK user id */
+    token: 'cc8aa272f840597e8046da7cb429e69704be761ed0755a4f4ee757769d586b5214d5e9442fe1440c4f2a0', /* VK access token */
     key: '' // VK Coin merchant key
 
 };
